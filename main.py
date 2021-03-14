bdd898ddd4b03ef6ad270e998fc4e8b8df15328b
Dzmitry Ratskevich <ratskevichdg@gmail.com>
2021-03-14 19:56:01 +0300

    show function was added

--- main.py
+++ main.py
@@ -282,6 +282,7 @@ def identity_matrix(num):
                 res_matrix[i][j] = 1
     return res_matrix
 
+
 def show():
     print(TEMPLATES['results_list'].format(
         answers = '\n'.join(str(i) for i in results_store)), 
@@ -292,26 +293,26 @@ def show():
         )    
 
 def main():
-    # absolute_value(-42)
-    # whole_number(123.456)
-    # times_two(21)
-    # string_length('Hello, hello!')
-    # num_of_words('The truth is out there')
-    # first_word('One, two, three')
-    # unique_letters('qweasjndjkeqewqwqw')
-    # vowels_num('qwertyuiop')
-    # is_palindrome('step on no pets')
-    # repeated_letters('Hello')
-    # pow_two_or_three(54)
-    # list_elem_sum([12, 54, 2, -12, 59])
-    # max_element([342, 5, 10000, 1, 34])
-    # low_letters('Hello, in future all my LETTERS will BE LOWER')
-    # genome_pair('TTTATGCCC')
-    # num_of_letter('g')
-    # to_binary(1024)
-    # ones_num_in_binary(42)
-    # censored_string('I\'ll be censored')
-    # identity_matrix(6)
+    absolute_value(-42)
+    whole_number(123.456)
+    times_two(21)
+    string_length('Hello, hello!')
+    num_of_words('The truth is out there')
+    first_word('One, two, three')
+    unique_letters('qweasjndjkeqewqwqw')
+    vowels_num('qwertyuiop')
+    is_palindrome('step on no pets')
+    repeated_letters('Hello')
+    pow_two_or_three(54)
+    list_elem_sum([12, 54, 2, -12, 59])
+    max_element([342, 5, 10000, 1, 34])
+    low_letters('Hello, in future all my LETTERS will BE LOWER')
+    genome_pair('TTTATGCCC')
+    num_of_letter('g')
+    to_binary(1024)
+    ones_num_in_binary(42)
+    censored_string('I\'ll be censored')
+    identity_matrix(6)
     show()
 
 if __name__ == '__main__':
